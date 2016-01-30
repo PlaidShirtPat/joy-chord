146dcf13857194c48703ba8c544d85d0bb8c2d02
Patrick <pat@plaidshirt.io>
2016-01-30 15:54:54 -0600

    Initial commit

---
+++ joy-chords.js
@@ -0,0 +1,65 @@
+
+$(document).ready(function(){
+  
+  var loopRef;
+  var activeButtonClass = "controller__button--pressed";
+
+  var buttonMappings = {
+    0: 'a',
+    1: 'b',
+    2: null,
+    3: 'x',
+    4: 'y',
+    5: null,
+    6: 'l1',
+    7: 'r1',
+    8: 'l2',
+    9: 'r2',
+    10: 'select',
+    11: 'start',
+    12: null,
+    13: 'lstick',
+    14: 'rstick',
+  }
+
+  var axisMappings = {
+    0: '',
+    1: '',
+    2: '',
+    3: '',
+    4: '',
+    5: '',
+    6: '',
+    7: '',
+  }
+
+  var buttonPressed = function (b) {
+    if (typeof(b) == "object") {
+      return b.pressed;
+    }
+    return b == 1.0;
+  }
+
+  var drawButtonStates = function(gp){
+    for(i=0; i<gp.buttons.length; i++){
+      if(buttonMappings[i]){
+        if(buttonPressed(gp.buttons[i]))
+          $('#btn-'+buttonMappings[i]).addClass(activeButtonClass);
+        else
+          $('#btn-'+buttonMappings[i]).removeClass(activeButtonClass);
+      }
+    }
+  }
+
+  var gameLoop = function(){
+
+    gp = navigator.getGamepads()[0]
+    if(!gp) return;
+
+    drawButtonStates(gp);
+  }
+
+  setInterval(gameLoop, 10);
+
+
+});
